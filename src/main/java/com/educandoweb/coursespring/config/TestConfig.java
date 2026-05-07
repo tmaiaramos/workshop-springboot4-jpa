62eaf1ed0e3c7e8098c62284faa6d7f8fbf65cd0
Tiago Maia Ramos <tramos@mpmg.mp.br>
2026-05-06 23:10:41 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/coursespring/config/TestConfig.java
+++ src/main/java/com/educandoweb/coursespring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.coursespring.entities.Category;
 import com.educandoweb.coursespring.entities.Order;
 import com.educandoweb.coursespring.entities.OrderItem;
+import com.educandoweb.coursespring.entities.Payment;
 import com.educandoweb.coursespring.entities.Product;
 import com.educandoweb.coursespring.entities.User;
 import com.educandoweb.coursespring.entities.enums.OrderStatus;
@@ -106,5 +107,9 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
